49012dbffd6680d4f801ef4dfd4f3a0fb9b2f31e
Vyara Todorova <vyaratodorova95@gmail.com>
2020-02-26 18:13:14 +0000

    model update - trial 3

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,10 +2,8 @@ class Search < ApplicationRecord
   belongs_to :country
   
   def self.search(term)
-    if term
+    
       WHERE("country.name::text LIKE ?","%#{term}%")
-    else
-      all
-    end
+  
   end
 end
